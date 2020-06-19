974d6f1e066491564b33fb0bf061876ce1e0d9a6
bradcownden <cowndenb@gmail.com>
2020-06-18 22:55:09 -0500

    Revert "Timings from CompilerGF462 data on laptop and V100 card"
    This reverts commit 44a5a4225d8036ad8fe243042a8128f129ca8674.

--- Output/TimingPlot.py
+++ Output/TimingPlot.py
@@ -22,20 +22,19 @@ dcode = 'utf_16_le'
 def readData(infile):
     t_factor, t_solve = [], []
     if os.path.isfile(infile):
-        # Try reading with windows encoding
-        with codecs.open(infile, 'r', encoding=dcode) as f:
-            for line in f:
-                # Read time to factor matrix on GPU
-                if "GPU factoring time" in line:
-                    line = line.split()
-                    t_factor.append(float(line[-2]))
-                elif "GPU solve time" in line:
-                    line = line.split()
-                    t_solve.append(float(line[-2]))
-                else:
-                    pass
-        # If windows codecs doesn't work, try without it
-        if len(t_solve) == 0:
+        if infile.split("/")[-1] == 'FullTimings.txt':
+            with codecs.open(infile, 'r', encoding=dcode) as f:
+                for line in f:
+                    # Read time to factor matrix on GPU
+                    if "GPU factor timing" in line:
+                        line = line.split()
+                        t_factor.append(float(line[-2]))
+                    elif "GPU solve time" in line:
+                        line = line.split()
+                        t_solve.append(float(line[-2]))
+                    else:
+                        pass
+        else:
             with open(infile, 'r') as f:
                 for line in f:
                     # Read time to factor matrix on GPU
@@ -59,54 +58,49 @@ def main(datadir):
     # Data files
     d1file = datadir + "FullTimings.txt"
     d2file = datadir + "cedar_timings.txt"
-    d3file = datadir + "cedar_v100_timings.txt"
 
     fig = plt.figure()
-    gs = fig.add_gridspec(1, 3)
+    gs = fig.add_gridspec(2, 3)
 
     # Laptop timings
     fig_ax1 = fig.add_subplot(gs[0,0:2])
     factor1, solve1 = readData(d1file)
-    fig_ax1.plot(np.arange(len(solve1)), solve1, '.', color='C0', label=r"Laptop: Quadro RTX 3000")
+    fig_ax1.plot(np.arange(len(solve1)), solve1, '.', color='C0')
+    fig_ax1.set_title(r"Laptop: solve $R\mathbf{X} = \mathbf{B}Q^{-1}$")
 
-    # Cedar_P100 timings
+    # Cedar timings
+    fig_ax2 = fig.add_subplot(gs[1,0:2])
     factor2, solve2 = readData(d2file)
-    fig_ax1.plot(np.arange(len(solve2)), solve2, '.', color='C3', label=r"P100 GPU")
-
-    # Cedar_V100 timings
-    factor3, solve3 = readData(d3file)
-    fig_ax1.plot(np.arange(len(solve3)), solve3, '.', color='C2', label=r"V100 GPU")
+    fig_ax2.plot(np.arange(len(solve2)), solve2, '.', color='C3')
+    fig_ax2.set_title(r"P100 GPU: solve $R\mathbf{X} = \mathbf{B}Q^{-1}$")
 
     # Matrix factoring
-    fig_ax2 = fig.add_subplot(gs[0,-1:])
-    fig_ax2.bar(r"$A=QR$", factor1, color='C0', label=r"Laptop")
-    fig_ax2.bar(r"$A=QR$", factor3, color='C2', label=r"V100 GPU")
-    fig_ax2.bar(r"$A=QR$", factor2, color='C3', label=r"P100 GPU")
-    fig_ax2.legend(bbox_to_anchor=(0., 1.02, 1., .102), loc='lower left',
+    fig_ax3 = fig.add_subplot(gs[:,-1:])
+    fig_ax3.bar(r"$A=QR$", factor1, color='C0', label=r"Laptop")
+    fig_ax3.bar(r"$A=QR$", factor2, color='C3', label=r"P100 GPU")
+    #fig_ax3.set_title(r"Factor matrix $A$")
+    fig_ax3.legend(bbox_to_anchor=(0., 1.02, 1., .102), loc='lower left',
            ncol=1, mode="expand", borderaxespad=0.)
 
-    f_ymax = max(factor1[0], max(factor2[0], factor3[0]))
-    f_ymin = min(factor1[0], min(factor2[0], factor3[0]))
-    s_ymax = max(np.max(solve1), max(np.max(solve2), np.max(solve3)))
-    s_ymin = min(np.min(solve1), min(np.min(solve2), np.min(solve3)))
+    f_ymax = max(factor1[0], factor2[0])
+    f_ymin = min(factor1[0], factor2[0])
+    s_ymax = max(np.max(solve1), np.max(solve2))
+    s_ymin = min(np.min(solve1), np.min(solve2))
     
     # Add axis labels
-    fig_ax1.set_ylabel(r"Time per solve $R\mathbf{x} = \mathbf{b}Q^{-1}$ (ms)")
-    fig_ax1.set_xlabel("Time Step")
-    fig_ax1.grid(True)
-    fig_ax1.legend(loc='best')
-    fig_ax1.set_ylim(0.0, s_ymax * 1.1)
-    fig_ax1.legend(bbox_to_anchor=(0., 1.02, 1., .102), loc='lower left',
-           ncol=2, mode="expand", borderaxespad=0.)
-
-    fig_ax2.set_ylabel("Time (ms)")
-    fig_ax2.set_ylim(0.0, f_ymax * 1.1)
-    fig_ax2.grid(True)
+    for axis in [fig_ax1, fig_ax2, fig_ax3]:
+        axis.set_ylabel("Time (ms)")
+        axis.grid(True)
+    for axis in [fig_ax1, fig_ax2]:
+        axis.set_xlabel("Time Step")
+        axis.set_ylim(0.0, s_ymax * 1.1)
+    for axis in [fig_ax3]:
+        axis.set_ylim(0.0, f_ymax * 1.1)
     
     plt.tight_layout()
     outfile = datadir + "FullTimings.pdf"
     plt.savefig(outfile, transparent=True, format='pdf', bbox='inches')
-    #plt.show()
+    plt.show()
 
 ###########################
 ###########################
