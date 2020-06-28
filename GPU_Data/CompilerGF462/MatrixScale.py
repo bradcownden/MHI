d8c7323b97a57782c7f86d69b9ac5b66695ed412
bradcownden <cowndenb@gmail.com>
2020-06-28 17:21:40 -0500

    Code and results for scaling up the size of the data set

--- GPU_Data/CompilerGF462/MatrixScale.py
+++ GPU_Data/CompilerGF462/MatrixScale.py
@@ -5,6 +5,7 @@ at each time step
 
 import numpy as np
 import os, sys, datetime
+import codecs
 
 #####################################
 #####################################
@@ -27,6 +28,7 @@ def readMatrix(infile):
                 #print(line)
                 pass
             elif len(line) == 3:
+                # print("Reading matrix data with key (%d, %d)" % (int(line[0]), int(line[1])))
                 matdata[(int(line[0]), int(line[1]))] = float(line[2])
             else:
                 #print("ERROR: unexpected data line")
@@ -46,17 +48,28 @@ def readData(infile):
 
     return indata
 
-# Write out vector data
+# Write out vector data with Linux encodings
 def writeVector(data, tstep, factor):
     fout = "SysVecB_n" + str(factor) + "_t" + str(tstep) + ".txt"
-    with open(MAINDIR + fout, 'a') as f:
-        for val in data:
-            f.write("%1.14e\n" % val)
+    # Append if already existing
+    if os.path.isfile(fout):
+        with open(MAINDIR + fout, 'a') as f:
+            for val in data:
+                f.write("%1.14e\n" % val)
+    else:
+        with open(MAINDIR + fout, 'w+') as f:
+            for val in data:
+                f.write("%1.14e\n" % val)
 
 # Write non-zero entries to .mtx file in column-ordered list
+# with Linux encodings
 def writeMatrix(outfile, row, col, val):
-    with open(outfile, 'a') as f:
-        f.write("%d %d %1.12e\n" % (row, col, val))
+    if isinstance(row, int) and isinstance(col, int) and isinstance(val, float):
+        with open(outfile, 'a') as f:
+            f.write("%d %d %1.12e\n" % (row, col, val))
+    else:
+        print("ERROR: writeMatrix did not recieve the correct data types.")
+        print("Should be (int) (int) (float) but got", type(row), type(col), type(val))
 
 # Write header to .mtx file
 def writeIntro(outfile, size, nnz):
@@ -70,9 +83,9 @@ def writeIntro(outfile, size, nnz):
         f.write("%--------------------------------------------------------------\n")
         f.write("%d %d %d\n" % (size, size, nnz))
 
-# Clean the w+ files from any previous runs
-def cleanVectors(dirpath):
-    files = [x for x in os.listdir(dirpath) if "SysVecB_n" in x]
+# Clean the existing files from any previous runs
+def cleanVectors(dirpath, factor):
+    files = [x for x in os.listdir(dirpath) if "SysVecB_n" + str(factor) in x]
     for f in files:
         os.remove(f)
 
@@ -86,7 +99,7 @@ def main(dirpath, factor):
     # length (factor * n)
 
     print("Multiplying dataset by a factor of %d..." % factor)
-    mat_out = dirpath + "sysMat_n" + str(factor) + "_t1.mtx"
+    mat_out = dirpath + "sysMatA_n" + str(factor) + "_t1.mtx"
     if os.path.exists(mat_out):
         pass
     else:
@@ -94,24 +107,35 @@ def main(dirpath, factor):
         # Get the number of non-zero entries
         nnz = len(matrix)
         new_mat = {} # Empty matrix
-        new_mat.update(matrix) # Add existing matrix values to scaled matrix
         # Add duplicate values to scaled-up matrix
-        for key in matrix.keys():
-            new_mat[(key[0] * factor, key[1] * factor)] = matrix.get(key)
+        for n in range(factor):
+            #print("Loop num", n)
+            for key in matrix.keys():
+                #print("Adding (%d, %d) = %f to new matrix" % (int(key[0] + (n * LINES)), 
+                #    int(key[1] + (n * LINES)), matrix.get(key)))
+                new_mat[(int(key[0] + (n * LINES)), int(key[1] + (n * LINES)))] = matrix.get(key)
 
+        #print(new_mat)
+        # Test the number of entries in the new matrix against the 
+        # number of entries in the old matrix
+        print(float(len(new_mat))/float(len(matrix)))   # Should be float(factor)
+        # Test the dimensions of the new matrix against the old matrix
+        n, m = sorted(new_mat.keys(), reverse=True)[0]
+        print("(%d, %d)" % (n,m))   # Should be (int(factor * LINES), int(factor * LINES))
         # Write the new matrix to file
-        writeIntro(mat_out, LINES * factor, nnz * factor)
+        writeIntro(mat_out, n, len(new_mat))
         ii = 0
         for key in sorted(new_mat.keys(), key = lambda x: x[1]):
             print("\rWrote %d%% of data." % int(ii / (nnz * factor) * 100),
                 end='')
+            #print("Writing key set (%d, %d)" % (key[0], key[1]))
             writeMatrix(mat_out, key[0], key[1], new_mat.get(key))
             ii += 1
 
         print("\rWrote 100%% of data.\n", end='')
         print("Wrote factored matrix to", mat_out)
 
-    cleanVectors(dirpath)
+    cleanVectors(dirpath, factor)
 
     ii = 1
     while True:
